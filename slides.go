73aaff61cae6901d4fce0a439c8e498b88b26391
Jim Reed <jim.reed@qlik.com>
2020-06-14 17:06:45 -0400

    added a drawing to a slide
    added a drawing to a slide

--- slides.go
+++ slides.go
@@ -1,23 +1,28 @@
 package slides
 
 import (
-    "bufio"
-	"strings"
-	"os"
+	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/jimareed/drawing"
 )
 
 // single slide
 type Slide struct {
-	title string
-	bullets []string
+	title    string
+	bullets  []string
 	hasImage bool
+	Drawing  drawing.Drawing
 }
 
 type SlideDeck struct {
-	Title string
-	slides []Slide
-	text []string
+	Title  string
+	Slides []Slide
+	text   []string
 }
 
 func Read(path string) (SlideDeck, error) {
@@ -28,19 +33,19 @@ func Read(path string) (SlideDeck, error) {
 	filename := path + "/README.md"
 
 	f, err := os.Open(filename)
-    if err != nil {
-        return deck, err
-    }
-    defer func() {
-        if err = f.Close(); err != nil {
-    }
-    }()
-    s := bufio.NewScanner(f)
-    for s.Scan() {
+	if err != nil {
+		return deck, err
+	}
+	defer func() {
+		if err = f.Close(); err != nil {
+		}
+	}()
+	s := bufio.NewScanner(f)
+	for s.Scan() {
 		deck.text = append(deck.text, s.Text())
-    }
+	}
 	err = s.Err()
-	
+
 	if len(deck.text) > 0 {
 		title := deck.text[0]
 		if strings.HasPrefix(title, "# ") {
@@ -49,32 +54,42 @@ func Read(path string) (SlideDeck, error) {
 		deck.Title = title
 	}
 
+	filename = path + "/00.draw"
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	text := string(content)
+
+	deck.Slides = append(deck.Slides, Slide{})
+	deck.Slides[0].Drawing, err = drawing.FromString(text)
+
 	return deck, err
 }
 
-func Write(deck SlideDeck, path string) (error) {
+func Write(deck SlideDeck, path string) error {
 
 	filename := path + "/README.md"
 
-    f, err := os.Create(filename)
-    if err != nil {
-        fmt.Println(err)
-                f.Close()
-        return err
-    }
+	f, err := os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return err
+	}
 
-    for _, v := range deck.text {
-        fmt.Fprintln(f, v)
-        if err != nil {
-            fmt.Println(err)
-            return err
-        }
-    }
-    err = f.Close()
-    if err != nil {
-        fmt.Println(err)
-        return err
-    }
+	for _, v := range deck.text {
+		fmt.Fprintln(f, v)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+	}
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
 
 	return nil
 }
